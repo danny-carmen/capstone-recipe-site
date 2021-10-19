94050bad954cf20c1c62043147f52f4d5cb4e279
danny-carmen <danny-carmen@live.com>
2021-10-19 14:06:06 -0600

    Removed comments

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -3,7 +3,6 @@ const { countDocuments } = require("../models/recipe.model");
 let Recipe = require("../models/recipe.model");
 
 router.route("/").get((req, res) => {
-  console.log("From Recipes");
   Recipe.countDocuments({}, function (err, count) {
     Recipe.find()
       .sort({ updatedAt: -1 })
